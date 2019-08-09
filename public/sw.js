072836537dd26931ad5c02849be35c80b1bbbb5d
Guido Kleine <33725136+GuidoKleine@users.noreply.github.com>
2019-08-09 10:21:18 +0200

    almost able to get cached storage when offline

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,31 @@
 // install server worker to browser
-self.addEventListener('install', function(event) {
-    console.log('[Service worker] Installing service worker ...', event);    
+self.addEventListener('install', function (event) {
+    console.log('[Service worker] Installing service worker ...', event);
+    event.waitUntil(
+        caches.open('static')
+            .then(function (cache) {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.add('/src/js/app.js')
+            })
+    )
 })
 
 // activates server worker in browser
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     console.log('[Service worker] Activating service worker ...', event);
-    return self.clients.claim();    
+    return self.clients.claim();
 })
 
 // catches all fetch request from browser going through service worker
-self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match() 
+      .then(function(response) {
+        if (response) {
+            return response; 
+        } else {
+            return fetch(event.request);
+        }
+      }) 
+    );
 })
